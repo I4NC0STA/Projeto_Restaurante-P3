312bf192aae0a084fa77622c49a46385c62b21b4
I4NC0STA <iancosta2026@gmail.com>
2026-06-07 19:08:23 -0300

    faze alfa do cardapio

---
+++ site/script.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll(".slide");
+let atual = 0;
+
+setInterval(() => {
+
+    slides[atual].classList.remove("active");
+
+    atual++;
+
+    if(atual >= slides.length){
+        atual = 0;
+    }
+
+    slides[atual].classList.add("active");
+
+}, 4000);
